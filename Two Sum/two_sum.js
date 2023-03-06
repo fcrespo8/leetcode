e2e6d000d6abdee9038eb43c251061d7d8b1e0ec
Francisco Crespo <fcrespo8@hotmail.com>
2023-03-06 18:23:50 +0100

    Javacript file done with two dif answers

--- Two Sum/two_sum.js
+++ Two Sum/two_sum.js
@@ -2,32 +2,26 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
- */
+//  */
+
+// var twoSum = function(nums, target) {
+//   for (let i1 = 0; i1 < nums.length; i1++) {
+//       for (let i2 = i1 + 1; i2 < nums.length; i2++) {
+//           if (nums[i1] + nums[i2] == target) {
+//               return [i1, i2]
+//           }
+//       }
+//   }
+// };
 
 var twoSum = function(nums, target) {
-  for (let i1 = 0; i1 < nums.length; i1++) {
-      for (let i2 = i1 + 1; i2 < nums.length; i2++) {
-          if (nums[i1] + nums[i2] == target) {
-              return [i1, i2]
-          }
-      }
-  }
-};
-
-nums = [2,7,11,15]
-target = 9
-
-var twoSum = function(nums, target) {
-  const indices = new Map(); // empty
-
-  for (let index = 0; index < nums.length; index++) {
-      const complement = target - nums[index]; // comp = 9 - 2 = 7
-      //comp = 9-7 = 2
-
-      if (indices.has(complement)) { // false - true
-          return [indices.get(complement), index] // [2, 0]
-      }
-
-      indices.set(nums[index], index) // [2, ]
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let num1 = nums[i] //num1=2--7
+    let num2 = target - num1 //num2=7--2
+    if (map.has(num2)){
+      return [map.get(num2), i]//return array with the first index and the index of num2
+    }
+    map.set(num1, i);//map = [2]
   }
 };
