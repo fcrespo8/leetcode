30f6103b6d620bd312146bc6373b565685006740
Francisco Crespo <fcrespo8@hotmail.com>
2023-03-13 19:11:17 +0100

    Cover Hole

--- 15 - Permutations/permutations.js
+++ 15 - Permutations/permutations.js
@@ -3,20 +3,40 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-  let result = []
+  const result = []
   if (nums.length === 1) {
       result.push(nums)
       return result
   }
-  result.push(nums)
-  let aux = []
-  for(let i=0; i < nums.length; i++){
-      for(let j=i+1; j < nums.length; j++){
-          aux.push(nums[j])
-      }
-      aux.push(nums[i])
-      result.push(aux)
-      return result
+  const dfs = (i, nums, aux) => {
+    if (i >= nums.length){
+      result.push(aux.join(''));
+      return;
+    }
+    for(let num of nums){
+      aux.push(num);
+      dfs(i + 1, nums, aux);
+      aux.pop();
+    }
   }
-  return result
+  dfs(0, nums, [])
+  return result;
 };
+// var permute = function(nums) {
+//   let result = []
+//   if (nums.length === 1) {
+//       result.push(nums)
+//       return result
+//   }
+//   result.push(nums)
+//   let aux = []
+//   for(let i=0; i < nums.length; i++){
+//       for(let j=i+1; j < nums.length; j++){
+//           aux.push(nums[j])
+//       }
+//       aux.push(nums[i])
+//       result.push(aux)
+//       return result
+//   }
+//   return result
+// };
